af52d75f68974454acb493a398f96915b6d18192
Taylor <taylorgon@gmail.com>
2020-02-10 23:30:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array 
-   [heart, coffee, coding, napping]
+   ["heart", "coffee", "coding", "napping"]
 end
 
 def add_element_to_end_of_array(array, element)
